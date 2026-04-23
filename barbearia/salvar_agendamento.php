e8d8b7da281294d96997ad9ce3dcc1f8d6399299
Gustavo_Nunes <gustavo.r.sousa@icloud.com>
2026-04-23 12:29:34 -0300

    Create salvar_agendamento.php

---
+++ barbearia/salvar_agendamento.php
@@ -0,0 +1,35 @@
+<?php
+include 'conexao.php';
+
+$nome = $_POST['nome'];
+$telefone = $_POST['telefone'];
+$servico_id = $_POST['servico_id'];
+$barbeiro_id = $_POST['barbeiro_id'];
+$data = $_POST['data'];
+$horario = $_POST['horario'];
+
+// Junta data e horário no formato que o banco espera
+$data_hora = $data . ' ' . $horario . ':00';
+
+// Verifica se já existe agendamento nesse horário com esse barbeiro
+$verifica = mysqli_query($conn, "SELECT id FROM agendamentos 
+    WHERE barbeiro_id = $barbeiro_id 
+    AND data_hora = '$data_hora'");
+
+if (mysqli_num_rows($verifica) > 0) {
+    header('Location: agendar.php?erro=horario_ocupado');
+    exit;
+}
+
+// Salva o agendamento
+$sql = "INSERT INTO agendamentos (barbeiro_id, servico_id, cliente_nome, cliente_telefone, data_hora) 
+        VALUES ($barbeiro_id, $servico_id, '$nome', '$telefone', '$data_hora')";
+
+if (mysqli_query($conn, $sql)) {
+    header('Location: confirmacao.php');
+    exit;
+} else {
+    header('Location: agendar.php?erro=geral');
+    exit;
+}
+?>
